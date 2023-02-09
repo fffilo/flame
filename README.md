7bd8a2cf714e80a65b149678537d0897b88840a9
Franjo Filo <fffilo666@gmail.com>
2023-02-09 16:32:32 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Flame
+=====
+
+Flame: creates mp3 audio files from stdin.
+
+## Options
+
+Flame is [lame](https://lame.sourceforge.io/) wrapper, so it will accept all the options lame does:
+
+    # all flac files in current directory:
+    # encode with fixed bit rate jstereo 128kbs encoding
+    find . -maxdepth 1 -type f -iname "*.flac" | flame -b 128
+
+    # all waw files in current directory:
+    # encode with fixed bit rate jstereo 128 kbps encoding, highest quality
+    find . -maxdepth 1 -type f -iname "*.wav" | flame -q 0 -b 128
+
+    # all audio files in /home directory, recursively:
+    # encode with the standard preset to /tmp directory
+    find /home -type f | flame --preset standard --out-dir /tmp
+
+## Install
+
+    sudo wget https://raw.githubusercontent.com/fffilo/flame/master/src/flame.sh -O /usr/local/bin/flame
+    sudo chmod +x /usr/local/bin/flame
+
+## Uninstall
+
+    sudo rm /usr/local/bin/flame
---
+++ src/flame.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Show help and exit.
+function show_help () {
+    echo "Flame: creates mp3 audio files from stdin."
+    echo ""
+    echo "https://github.com/fffilo/flame"
+    echo ""
+
+    exit 0
+}
+
+# Show error and exit with non-zero status code.
+function error_log () {
+    echo "flame: $1" >&2
+
+    exit 1
+}
+
+# Check dependency.
+if ! [ -x "$(command -v lame)" ]; then
+    error_log "lame is not installed"
+fi
+
+# Check if pipe exists on stdin.
+if [ ! -p /dev/stdin ]; then
+    show_help
+fi
+
+# Read stdin line by line.
+files=()
+while IFS= read line; do
+    if [ ! -f "$line" ]; then
+        error_log "file does not exist $line"
+    fi
+
+    # Append audio file to files list.
+    if [[ "$(file --mime-type -b "$line")" == audio/* ]]; then
+        files+=("$line")
+    fi
+done
+
+# Destination directory.
+outdir=""
+args=("$@")
+for i in "${!args[@]}"; do
+    if [ "${args[$i]}" == "--out-dir" ]; then
+        outdir="${args[$((i+1))]}"
+        if [ -z "$outdir" ]; then
+            error_log "output directory not provided"
+        elif [ ! -d "$outdir" ]; then
+            error_log "output directory does not exist $outdir"
+        fi
+
+        outdir="$(realpath "$outdir")"
+
+        break
+    fi
+done
+
+# Iterate files.
+for i in "${files[@]}"; do
+    src="$(realpath "$i")"
+    dst="${src%.*}.mp3"
+
+    if [ "$outdir" == "/" ]; then
+        dst="/$(basename "$dst")"
+    elif [ ! -z "$outdir" ]; then
+        dst="${outdir}/$(basename "$dst")"
+    fi
+
+    # Execute lame on file.
+    lame ${@} "${src}" "${dst}"
+
+    # Exit on fail.
+    status=$?
+    if [ $status -ne 0 ]; then
+        exit $status
+    fi
+done
